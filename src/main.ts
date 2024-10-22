28c0f8a0ad54f7ab07ee9fa6de19d530b3bfabe4
Tony Pau <topau@ucsc.edu>
2024-10-22 14:55:23 -0700

    Step 0 set up. GameTitle name change

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Artistic Playground";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
