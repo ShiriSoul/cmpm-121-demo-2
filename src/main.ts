6ab7b6213a617a859bc140202c89e82b5b0aaa7c
Tony Pau <topau@ucsc.edu>
2024-10-22 15:04:03 -0700

    Step 1 complete.

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,14 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const title = document.createElement("h1");
+title.textContent = APP_NAME;
+app.appendChild(title);
+
+// canvas element creation
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.id = "PlaygroundCanvas";
+app.appendChild(canvas);
